80dbcb091f30b31473edfa0aa9050046278260e8
SalveMundiProd <salvemundiproductions@gmail.com>
2022-01-04 17:03:35 -0500

    saving to github

--- src/main/java/draylar/gofish/mixin/FishingBobberEntityRendererMixin.java
+++ src/main/java/draylar/gofish/mixin/FishingBobberEntityRendererMixin.java
@@ -1,48 +1,26 @@
 package draylar.gofish.mixin;
 
 import draylar.gofish.item.ExtendedFishingRodItem;
-import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.FishingBobberEntityRenderer;
-import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.projectile.FishingBobberEntity;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(FishingBobberEntityRenderer.class)
 public class FishingBobberEntityRendererMixin {
 
-    private PlayerEntity gofish_owner;
-
-    @Inject(
-            method = "render",
-            at = @At("HEAD")
+	@Redirect(
+	    method = "render",
+	    at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;isOf(Lnet/minecraft/item/Item;)Z")
     )
-    private void storeContext(FishingBobberEntity fishingBobberEntity, float f, float g, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, CallbackInfo ci) {
-        gofish_owner = fishingBobberEntity.getPlayerOwner();
-    }
-
-    @ModifyVariable(
-            method = "render",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;getHandSwingProgress(F)F"),
-            index = 12
-    )
-    private int mod(int i) {
-        ItemStack itemStack = gofish_owner.getMainHandStack();
-
-        // previous condition was hit
-        if (itemStack.getItem() != Items.FISHING_ROD) {
-            if(itemStack.getItem() instanceof ExtendedFishingRodItem) {
-                // undo change
-                return -i;
-            }
+    private boolean itemIsRod(ItemStack itemStack, Item item) {
+        // if hand stack item is item original method looks for OR is new fishing rod
+        if (itemStack.getItem() == item || itemStack.getItem() instanceof ExtendedFishingRodItem) {
+            // return true
+            return true;
         }
-
-        return i;
+        return false;
     }
 }
